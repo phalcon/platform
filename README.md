8107ad58f95747a0563e5ece253a6641682f313a
Serghei Iakovlev <serghei@phalconphp.com>
2018-06-29 14:44:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Platform
+
+A foundation to build applications using Phalcon.
+
+## License
+
+Platform is an open-sourced software licensed under the [New BSD License][:license:].<br>
+© Phalcon Team
+
+[:license:]: https://github.com/phalcon/platform/blob/master/LICENSE.txt
---
+++ src/Domain/AbstractRepository.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+/*
+ +------------------------------------------------------------------------+
+ | Phalcon Platform                                                       |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2018 Phalcon Team                                        |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Platform\Domain;
+
+use Phalcon\Mvc\Model\ResultsetInterface;
+use Phalcon\Mvc\ModelInterface;
+
+/**
+ * Phalcon\Platform\Domain\AbstractRepository
+ *
+ * @package Phalcon\Platform\Domain
+ */
+abstract class AbstractRepository implements RepositoryInterface
+{
+    /** @var ModelInterface */
+    private $entity;
+
+    /**
+     * AbstractRepository constructor.
+     *
+     * @param ModelInterface $entity
+     */
+    public function __construct(ModelInterface $entity)
+    {
+        $this->entity = $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return ModelInterface
+     */
+    public function getEntity(): ModelInterface
+    {
+        return $this->entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param int|string|array $parameters
+     *
+     * @return ResultsetInterface
+     */
+    public function find($parameters = null): ResultsetInterface
+    {
+        $result = $this->getEntity()->find($parameters);
+
+        return $result ?: null;
+    }
+}
