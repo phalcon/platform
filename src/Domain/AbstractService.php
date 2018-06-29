aa462f7ff625552fa115545f07e044c80bd76ed5
Serghei Iakovlev <serghei@phalconphp.com>
2018-06-29 15:10:55 +0300

    Added initial domain service

---
+++ src/Domain/AbstractService.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+/*
+ +------------------------------------------------------------------------+
+ | Phalcon Platform                                                       |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2018 Phalcon Team                                        |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Platform\Domain;
+
+/**
+ * Phalcon\Platform\Domain\AbstractService
+ *
+ * @package Phalcon\Platform\Domain
+ */
+class AbstractService implements ServiceInterface
+{
+    /** @var RepositoryInterface */
+    private $repository;
+
+    /**
+     * AbstractRepository constructor.
+     *
+     * @param RepositoryInterface $repository
+     */
+    public function __construct(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return RepositoryInterface
+     */
+    public function getRepository(): RepositoryInterface
+    {
+        return $this->repository;
+    }
+}
---
+++ src/Domain/ServiceInterface.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+/*
+ +------------------------------------------------------------------------+
+ | Phalcon Platform                                                       |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2018 Phalcon Team                                        |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon\Platform\Domain;
+
+/**
+ * Phalcon\Platform\Domain\ServiceInterface
+ *
+ * @package Phalcon\Platform\Domain
+ */
+interface ServiceInterface
+{
+    /**
+     * Get related repository.
+     *
+     * @return RepositoryInterface
+     */
+    public function getRepository(): RepositoryInterface;
+}
